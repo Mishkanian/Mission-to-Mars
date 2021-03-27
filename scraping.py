ff9d7cc6c3aae3cb9679d240470e53cfc7790b64
Michael Mishkanian <77469811+Mishkanian@users.noreply.github.com>
2021-03-27 12:55:07 -0700

    Integrated MongoDB into the web app

--- scraping.py
+++ scraping.py
@@ -1,17 +1,28 @@
-# Import Splinter, BeautifulSoup, and Pandas
+# Imports
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Path to chromedriver
-#which chromedriver
+def scrape_all():
+    # Initiate headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()}
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
-# Set the executable path and initialize the chrome browser in splinter
-executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-browser = Browser('chrome', **executable_path)
 
 # Scrape Mars News
-
 def mars_news(browser):
     # Visit the mars nasa news site
     url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
@@ -70,4 +81,6 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html()
 
-browser.quit()
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
